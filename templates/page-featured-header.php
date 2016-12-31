e4929235ca33793a3c38b76714d494a367acfc15
ben <bwhitlock@cyberbility.com>
2016-12-31 00:24:31 -0500

    new templates folder

---
+++ templates/page-featured-header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: Featured Header
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package cyberbility-starter
+ */
+
+get_header(); ?>
+
+<div class="wrapper" id="page-wrapper">
+
+  <div  id="content" class="container">
+
+	<div class="header-img row">
+		<?php the_post_thumbnail(); ?>
+	</div>
+
+    <div class="row">
+
+      <div id="primary" class="col-md-12 content-area">
+
+       <main id="main" class="site-main" role="main">
+
+        <?php while ( have_posts() ) : the_post(); ?>
+          <?php get_template_part( 'templates/content', 'page' ); ?>                
+        <?php endwhile; // end of the loop. ?>
+        
+    </main><!-- #main -->
+
+  </div><!-- #primary -->
+
+</div><!-- .row -->
+
+</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
