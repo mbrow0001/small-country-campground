84d281e73bf5061bd0aed42bd25faa643727fd03
ben <bwhitlock@cyberbility.com>
2016-12-31 01:11:02 -0500

    added first custom header

---
+++ templates/page-header-aardvark.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Header (Aardvark)
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package cyberbility-starter
+ */
+
+get_template_part('admin/headers/header', 'aardvark'); ?>
+
+<div class="wrapper" id="page-wrapper">
+
+  <div  id="content" class="container">
+
+    <div class="row">
+
+      <div id="primary" class="col-md-12 content-area">
+
+       <main id="main" class="site-main" role="main">
+
+        <?php while ( have_posts() ) : the_post(); ?>
+          <?php get_template_part( 'templates/content', 'page' ); ?>                
+        <?php endwhile; // end of the loop. ?>
+        
+    </main><!-- #main -->
+
+  </div><!-- #primary -->
+
+</div><!-- .row -->
+
+</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
