a1a1bf761347ae8df00ccd3c35cc5cd5df39b517
ben <bwhitlock@cyberbility.com>
2017-01-02 23:32:13 -0500

    update gitignore

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,7 +6,7 @@
 function cyberbility_starter_scripts() {
 	wp_enqueue_style( 'cyberbility-starter-style', get_template_directory_uri() . '/css/style.css', array(), '0.0.1');
 	wp_enqueue_script('jquery'); 
-	wp_enqueue_script( 'cyberbility-starter-js', get_template_directory_uri() . '/js/theme.min.js', array(), '0.0.1', true );
+	wp_enqueue_script( 'cyberbility-starter-js', get_template_directory_uri() . '/js/theme.js', array(), '0.0.1', true );
 
 	 // Template Header - Caribou
 	wp_enqueue_style( 'header-dingo', get_template_directory_uri() . '/css/header-dingo.css', array(), '0.0.1' );
