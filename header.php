834a025832588fe44f102b3c1f3b24693cc4bc4d
Ben Whitlock <bwhitlock@cyberbility.com>
2017-01-06 15:59:02 -0500

    updated the header layout and style

--- header.php
+++ header.php
@@ -8,19 +8,19 @@
     *
     * @package cyberbility-starter
     */
-    ?><!DOCTYPE html>
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php wp_head(); ?>
-<link href="https://fonts.googleapis.com/css?family=Cutive|Raleway:400,900" rel="stylesheet">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css?family=Cutive|Raleway:400,900" rel="stylesheet">
 </head>
 <body <?php body_class(); ?>>
     <div id="page" class="hfeed site">
@@ -28,7 +28,30 @@
         <div class="wrapper-fluid wrapper-navbar">
             <a class="skip-link screen-reader-text sr-only" href="#content"><?php _e( 'Skip to content', 'cyberbility-starter' ); ?></a>
 
-            <nav class="navbar navbar-light navbar-full bg-faded container-fluid" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+                <div class="container-fluid">
+                    <div class="row text-xs-center head__contact">
+                        <div class="col-md-4">
+                            <p>
+                                Ready to Book? 
+                                <span><a href="#">Check Availability</a></span>
+                            </p>
+                        </div>
+                        <div class="col-md-4">
+                            <p>
+                                Talk to Our Team 
+                                <span><a href="tel:5409672431">(540) 967-2431</a></span>
+                            </p>
+                        </div>
+                        <div class="col-md-4">
+                            <p>
+                                Email A Question 
+                                <span><a href="mailto:camp@smallcountry.com">camp@smallcountry.com</a></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            <nav class="navbar navbar-full container-fluid" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
                 <div class="navbar-header">
                     <!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
                     <button class="navbar-toggler hidden-xl-up" type="button" data-toggle="collapse" data-target=".mobile">
@@ -48,78 +71,16 @@
                         'fallback_cb' => '',
                         'menu_id' => 'main-menu',
                         'walker' => new wp_bootstrap_navwalker()
-                        )
-                        ); ?>
-                    </nav><!-- .site-navigation -->       
-                    
-                    <div class="container-fluid">
-                    <div class="header__logo text-xs-center">
-                        <img src="http://cycat4.com/wp-content/uploads/2016/12/logo.png" alt="Small Country Campground Virginia Logo">
-                    </div>
+                    )
+                ); ?>
+            </nav><!-- .site-navigation -->       
 
-                        <div class="row text-xs-center head__contact">
-                            <div class="col-md-4">
-                                <p>
-                                    Ready to Book? 
-                                    <span><a href="#">Check Availability</a></span>
-                                </p>
-                            </div>
-                            <div class="col-md-4">
-                                <p>
-                                    Talk to Our Team 
-                                    <span><a href="tel:5409672431">(540) 967-2431</a></span>
-                                </p>
-                            </div>
-                            <div class="col-md-4">
-                                <p>
-                                    Email A Question 
-                                    <span><a href="mailto:camp@smallcountry.com">camp@smallcountry.com</a></span>
-                                </p>
-                            </div>
-                        </div>
-
-                        <div class="row head__address text-xs-center">
-                            Small Country Campground | 4400 Byrd Mill Road | Louisa VA 23093
-                        </div>
-
-                        <div class="row text-xs-center head__icons">
-                            <div class="col-md-2">
-                                <div class="head__icon">
-                                    Facebook
-                                    <i class="fa fa-twitter"></i>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <div class="head__icon">
-                                    Twitter
-                                    <i class="fa fa-twitter"></i>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <div class="head__icon">
-                                    Yelp
-                                    <i class="fa fa-twitter"></i>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <div class="head__icon">
-                                    Trip Advisor
-                                    <i class="fa fa-twitter"></i>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <div class="head__icon">
-                                    Instagram
-                                    <i class="fa fa-twitter"></i>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <div class="head__icon">
-                                    Dribble
-                                    <i class="fa fa-twitter"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-            </div><!-- .wrapper-navbar end -->
+            <div class="container-fluid">
+                <div class="header__logo text-xs-center">
+                    <img src="http://cycat4.com/wp-content/uploads/2016/12/logo.png" alt="Small Country Campground Virginia Logo">
+                </div>
+                <div class="row head__address text-xs-center">
+                    Small Country Campground | 4400 Byrd Mill Road | Louisa VA 23093
+                </div>
+            </div><!-- .container-fluid end -->
+        </div><!-- .wrapper-navbar end -->
