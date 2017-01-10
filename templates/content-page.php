a76a42db34c0fca70ab37237448598fd0ef91bd9
Ben Whitlock <bwhitlock@cyberbility.com>
2017-01-10 12:29:02 -0500

    update wts boxes

--- templates/content-page.php
+++ templates/content-page.php
@@ -10,9 +10,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-	</header><!-- .entry-header -->
+	<!-- <header class="entry-header">
+		<?php// the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+	</header> --><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
